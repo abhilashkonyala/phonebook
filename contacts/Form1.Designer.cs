3bb8441ff69b3d2015780df366c73eb27d40da82
abhilash <53434877+abhilashkonyala@users.noreply.github.com>
2021-05-11 17:59:58 +0530

    Add files via upload

---
+++ contacts/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace contacts
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_id = new System.Windows.Forms.TextBox();
+            this.txt_Mobile = new System.Windows.Forms.TextBox();
+            this.txt_Lname = new System.Windows.Forms.TextBox();
+            this.txt_Fname = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btn_save = new System.Windows.Forms.Button();
+            this.dtGrid = new System.Windows.Forms.DataGridView();
+            this.btn_update = new System.Windows.Forms.Button();
+            this.btn_del = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.txt_search = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dtGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txt_id
+            // 
+            this.txt_id.Enabled = false;
+            this.txt_id.Location = new System.Drawing.Point(169, 95);
+            this.txt_id.Name = "txt_id";
+            this.txt_id.Size = new System.Drawing.Size(100, 20);
+            this.txt_id.TabIndex = 0;
+            // 
+            // txt_Mobile
+            // 
+            this.txt_Mobile.Location = new System.Drawing.Point(169, 229);
+            this.txt_Mobile.Name = "txt_Mobile";
+            this.txt_Mobile.Size = new System.Drawing.Size(100, 20);
+            this.txt_Mobile.TabIndex = 1;
+            // 
+            // txt_Lname
+            // 
+            this.txt_Lname.Location = new System.Drawing.Point(169, 184);
+            this.txt_Lname.Name = "txt_Lname";
+            this.txt_Lname.Size = new System.Drawing.Size(100, 20);
+            this.txt_Lname.TabIndex = 2;
+            // 
+            // txt_Fname
+            // 
+            this.txt_Fname.Location = new System.Drawing.Point(169, 139);
+            this.txt_Fname.Name = "txt_Fname";
+            this.txt_Fname.Size = new System.Drawing.Size(100, 20);
+            this.txt_Fname.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(102, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(15, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "id";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(102, 146);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Firstname";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(102, 191);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Lastname";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(102, 236);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "phone";
+            // 
+            // btn_save
+            // 
+            this.btn_save.Location = new System.Drawing.Point(346, 139);
+            this.btn_save.Name = "btn_save";
+            this.btn_save.Size = new System.Drawing.Size(72, 65);
+            this.btn_save.TabIndex = 8;
+            this.btn_save.Text = "Save";
+            this.btn_save.UseVisualStyleBackColor = true;
+            this.btn_save.Click += new System.EventHandler(this.Btn_save_Click);
+            // 
+            // dtGrid
+            // 
+            this.dtGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtGrid.Location = new System.Drawing.Point(34, 271);
+            this.dtGrid.Name = "dtGrid";
+            this.dtGrid.Size = new System.Drawing.Size(497, 167);
+            this.dtGrid.TabIndex = 9;
+            this.dtGrid.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtGrid_CellContentClick);
+            // 
+            // btn_update
+            // 
+            this.btn_update.Location = new System.Drawing.Point(346, 86);
+            this.btn_update.Name = "btn_update";
+            this.btn_update.Size = new System.Drawing.Size(72, 29);
+            this.btn_update.TabIndex = 10;
+            this.btn_update.Text = "Update";
+            this.btn_update.UseMnemonic = false;
+            this.btn_update.UseVisualStyleBackColor = true;
+            this.btn_update.Click += new System.EventHandler(this.btn_update_Click);
+            // 
+            // btn_del
+            // 
+            this.btn_del.Location = new System.Drawing.Point(346, 226);
+            this.btn_del.Name = "btn_del";
+            this.btn_del.Size = new System.Drawing.Size(72, 23);
+            this.btn_del.TabIndex = 11;
+            this.btn_del.Text = "Delete";
+            this.btn_del.UseVisualStyleBackColor = true;
+            this.btn_del.Click += new System.EventHandler(this.btn_del_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(657, 94);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Search";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Firstname",
+            "Lastname",
+            "phone"});
+            this.comboBox1.Location = new System.Drawing.Point(704, 86);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(66, 21);
+            this.comboBox1.TabIndex = 13;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // txt_search
+            // 
+            this.txt_search.Location = new System.Drawing.Point(785, 87);
+            this.txt_search.Name = "txt_search";
+            this.txt_search.Size = new System.Drawing.Size(100, 20);
+            this.txt_search.TabIndex = 14;
+            this.txt_search.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txt_search_KeyUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(991, 450);
+            this.Controls.Add(this.txt_search);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btn_del);
+            this.Controls.Add(this.btn_update);
+            this.Controls.Add(this.dtGrid);
+            this.Controls.Add(this.btn_save);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_Fname);
+            this.Controls.Add(this.txt_Lname);
+            this.Controls.Add(this.txt_Mobile);
+            this.Controls.Add(this.txt_id);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_id;
+        private System.Windows.Forms.TextBox txt_Mobile;
+        private System.Windows.Forms.TextBox txt_Lname;
+        private System.Windows.Forms.TextBox txt_Fname;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btn_save;
+        private System.Windows.Forms.DataGridView dtGrid;
+        private System.Windows.Forms.Button btn_update;
+        private System.Windows.Forms.Button btn_del;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox txt_search;
+    }
+}
+
---
+++ contacts/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace contacts
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static string connectin = @"Data Source=ABHILASH\SQLEXPRESS;Initial Catalog=phonebook;Integrated Security=True";
+        SqlConnection conn = new SqlConnection(connectin);
+        private void Btn_save_Click(object sender, EventArgs e)
+        {
+           
+            string SqlCommand = "INSERT INTO tbl_Contacts(Firstname, Lastname, Phone) VALUES('" + txt_Fname.Text + "','" + txt_Lname.Text + "','" + txt_Mobile.Text + "')";
+            conn.Open();
+            SqlDataAdapter dtAP = new SqlDataAdapter(SqlCommand,conn);
+            DataSet dtSet = new DataSet();
+            dtAP.Fill(dtSet);
+            MessageBox.Show("data saved");
+            conn.Close();
+            loading_grid();
+    }
+
+
+        public void loading_grid()
+        {
+            string SqlCommand = "SELECT * FROM tbl_Contacts";
+            conn.Open();
+            SqlDataAdapter dtAP = new SqlDataAdapter(SqlCommand, conn);
+            DataTable dtTab = new DataTable();
+            dtAP.Fill(dtTab);
+            dtGrid.DataSource = dtTab;
+            conn.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex = 0;
+            loading_grid();
+        }
+
+        private void btn_update_Click(object sender, EventArgs e)
+        {
+            string SqlCommand = "UPDATE tbl_Contacts SET Firstname='" + txt_Fname.Text + "', Lastname='" + txt_Lname.Text + "', Phone='" + txt_Mobile.Text + "'WHERE id='"+txt_id.Text+"' ";
+            conn.Open();
+            SqlDataAdapter dtAP = new SqlDataAdapter(SqlCommand, conn);
+            DataSet dtSet = new DataSet();
+            dtAP.Fill(dtSet);
+            MessageBox.Show("data updated");
+            conn.Close();
+            loading_grid();
+            txt_id.Clear();
+            txt_Fname.Clear();
+            txt_Lname.Clear();
+            txt_Mobile.Clear();
+
+        }
+
+        private void dtGrid_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txt_id.Text = dtGrid.CurrentRow.Cells[0].Value.ToString();
+            txt_Fname.Text = dtGrid.CurrentRow.Cells[1].Value.ToString();
+            txt_Lname.Text = dtGrid.CurrentRow.Cells[2].Value.ToString();
+            txt_Mobile.Text = dtGrid.CurrentRow.Cells[3].Value.ToString();
+        }
+
+        private void btn_del_Click(object sender, EventArgs e)
+        {
+            string SqlCommand = "DELETE FROM tbl_Contacts WHERE id='" + txt_id.Text + "' ";
+            conn.Open();
+            SqlDataAdapter dtAP = new SqlDataAdapter(SqlCommand, conn);
+            DataSet dtSet = new DataSet();
+            dtAP.Fill(dtSet);
+            MessageBox.Show("DELETED");
+            conn.Close();
+            loading_grid();
+            txt_id.Clear();
+            txt_Fname.Clear();
+            txt_Lname.Clear();
+            txt_Mobile.Clear();
+
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void txt_search_KeyUp(object sender, KeyEventArgs e)
+        {
+            string SqlCommand = "SELECT * FROM tbl_Contacts WHERE(Firstname LIKE '" + txt_search.Text+"%') OR (Lastname LIKE '" + txt_search.Text+ "%') OR (Phone LIKE '" + txt_search.Text + "%')";
+            conn.Open();
+            SqlDataAdapter dtAP = new SqlDataAdapter(SqlCommand, conn);
+            DataTable dtTab = new DataTable();
+            dtAP.Fill(dtTab);
+            dtGrid.DataSource = dtTab;
+            conn.Close();
+          
+        }
+
+       
+    }
+}
+
+
+
+
